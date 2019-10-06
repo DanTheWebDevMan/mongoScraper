6b0dd0c9e7b9c43f2d8a4c03b02ec0506c554ade
Danniel Zelaya <dannielzelaya@gmail.com>
2019-10-05 22:07:43 -0500

    changed some of the stlyling to the page

--- public/app.js
+++ public/app.js
@@ -9,24 +9,24 @@ $(document).ready(function () {
             articles.forEach(article => {
                 //creating article in html
                 const articleContainer = $("<div></div>");
-                const headline = $("<p>" + article.headline + "</p>");
-                const summary = $("<p>" + article.summary + "</p>");
+                const headline = $("<h3>" + article.headline + "<h3>");
+                const summary = $("<p class= 'summary'>" + article.summary + "</p>");
                 const url = $(`<a href= "https://www.nytimes.com${article.url}">Go to article:</a>`);
                 //text area for user to comment
-                const textArea = $("<textarea></textarea>");
+                const textArea = $("<textarea>Insert Comment here</textarea>");
                 const addButton = $("<button class=\"submit\" >Submit Comment</button>");
                 const commentSection = $("<div></div>");
-                //taking all comment for specific article and appending to comment section
+                //taking all comments for specific article and appending to comment section
                 article.comment.forEach(comment => {
                     commentSection.append(`<p>${comment.body}</p>`);
-
+                    //adds delete button to comment section and function to remove comment by selected id
                     const deleteButton = $("<button class='delete-button'>Delete Comment</button>");
                     $(deleteButton).on("click", function (e) {
                         const commentId = comment._id;
                         fetch("/comments/" + commentId, {
                             method: "delete",
                         }).then(() => {
-                            //reloads page
+                            //reloads page to display without comment
                             window.location.reload();
                         })
                     })
@@ -35,8 +35,7 @@ $(document).ready(function () {
                 //on click event (e), grabbing value from text area and posting into DB
                 $(addButton).on("click", function (e) {
                     const comments = $(this).siblings("textarea").val();
-                    const id = article._id
-
+                    const id = article._id;
                     fetch("/articles/" + id, {
                         method: "post",
                         headers: {
@@ -51,7 +50,7 @@ $(document).ready(function () {
                     })
                 });
 
-                //appending elements
+                //appending all elements onto the page
                 articleContainer.append(headline);
                 articleContainer.append(summary);
                 articleContainer.append(url);
